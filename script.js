e68afe469324ba621d35c62c462c9d61e3876e52
chicken-ahm-12 <ahmeduni151@gmail.com>
2025-11-23 21:08:48 +0500

    Added contactus, suv, sedan aboutus pages and a map.

--- script.js
+++ script.js
@@ -3,3 +3,15 @@
     #CF0F47 -- red
     #09122C -- black
     #E8E8E8 -- white */
+
+
+const home_btn = document.getElementById('home_btn');
+const contact_btn = document.getElementById('contact_btn');
+
+home_btn.addEventListener('click', function(){
+    window.location.href = 'index.html';
+})
+
+contact_btn.addEventListener('click', function(){
+    window.location.href = 'contactus.html';
+})
