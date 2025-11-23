ec114704a806dbc902d85d98e81a1f6cccafd796
chicken-ahm-12 <ahmeduni151@gmail.com>
2025-11-23 21:48:49 +0500

    Completed.

--- script.js
+++ script.js
@@ -4,9 +4,14 @@
     #09122C -- black
     #E8E8E8 -- white */
 
+import $ from 'jquery'
 
 const home_btn = document.getElementById('home_btn');
 const contact_btn = document.getElementById('contact_btn');
+const submit_btn = document.getElementById('submit');
+const cont_area = document.getElementById('contArea');
+const cont_name = document.getElementById('contName');
+const navbarplaceholder = document.getElementById('navbar-placeholder')
 
 home_btn.addEventListener('click', function(){
     window.location.href = 'index.html';
@@ -14,4 +19,8 @@ home_btn.addEventListener('click', function(){
 
 contact_btn.addEventListener('click', function(){
     window.location.href = 'contactus.html';
+})
+submit_btn.addEventListener('click', function(){
+    cont_name.value = '';
+    cont_area.value = '';
 })
